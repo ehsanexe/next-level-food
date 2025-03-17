1f82d240249ce65b20a639742c206c1c529c714d
Ehsan <ehsans.contractor@elementsgs.com>
2025-03-17 14:33:29 +0500

    update page.js, page.js, image-slideshow.js and main-header.js

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function Meals() {
   return (
-    <div>Meals</div>
-  )
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
