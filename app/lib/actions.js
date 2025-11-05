28c1319d97ab24e48f81b24bdcbabc82facd17f3
ehsanexe <ehsanexe01@gmail.com>
2025-11-05 17:16:26 +0500

    add validation for meal sharing and create error component

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -3,6 +3,13 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+const validateText = (text) => {
+  if (!text || text.trim().length === 0) {
+    return false;
+  }
+  return true;
+};
+
 export const shareMeal = async (formData) => {
   const meal = {
     creator: formData.get("name"),
@@ -12,6 +19,20 @@ export const shareMeal = async (formData) => {
     instructions: formData.get("instructions"),
     image: formData.get("image"),
   };
+
+  if (
+    !validateText(meal.name) ||
+    !validateText(meal.email) ||
+    !meal.email.includes("@") ||
+    !validateText(meal.title) ||
+    !validateText(meal.summary) ||
+    !validateText(meal.instructions) ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid input ");
+  }
+
   await saveMeal(meal);
   redirect("/meals");
 };
